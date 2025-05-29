a98154b3c51482f6d9967b4080b67395734e80fa
salahmohsen <salahmmohsen@gmail.com>
2025-05-29 19:37:21 +0300

    Refactor ESLint configuration: update import statements, add custom import sorting rules, and change endOfLine setting to 'lf'

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,14 @@
-import { FlatCompat } from '@eslint/eslintrc'
-import perfectionist from 'eslint-plugin-perfectionist'
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc';
+import perfectionist from 'eslint-plugin-perfectionist';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname
-})
+});
 
 const eslintConfig = [
   ...compat.config({
@@ -22,29 +22,61 @@ const eslintConfig = [
     rules: {
       'project-structure/independent-modules': 'error',
       ...perfectionist.configs['recommended-natural'].rules,
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'import/order': 'off',
       'jsx-a11y/alt-text': 'warn',
       'jsx-a11y/aria-props': 'warn',
       'jsx-a11y/aria-proptypes': 'warn',
       'jsx-a11y/aria-unsupported-elements': 'warn',
       'jsx-a11y/role-has-required-aria-props': 'warn',
       'jsx-a11y/role-supports-aria-props': 'warn',
+      'perfectionist/sort-imports': [
+        'error',
+        {
+          customGroups: [
+            {
+              elementNamePattern: ['^react$', '^react-.+'],
+              groupName: 'react',
+              selector: 'import'
+            },
+            {
+              elementNamePattern: ['^next$', '^next-.+'],
+              groupName: 'next',
+              selector: 'import'
+            }
+          ],
+          groups: [
+            ['react', 'next'],
+            'type-import',
+            ['value-builtin', 'value-external'],
+            'type-internal',
+            'value-internal',
+            ['type-parent', 'type-sibling', 'type-index'],
+            ['value-parent', 'value-sibling', 'value-index'],
+            'ts-equals-import',
+            'unknown'
+          ],
+          internalPattern: ['^@/.+', '^~/.+'],
+          newlinesBetween: 'always',
+          order: 'asc',
+          type: 'natural'
+        }
+      ],
       'prettier/prettier': [
         'error',
         {
           arrowParens: 'always',
-          endOfLine: 'auto',
+          endOfLine: 'lf',
           plugins: ['prettier-plugin-tailwindcss'],
           printWidth: 80,
-          semi: false,
           singleQuote: true,
           tabWidth: 2,
-          trailingComma: 'none'
-        },
-        {
+          trailingComma: 'none',
           usePrettierrc: false
         }
       ],
-      'react/react-in-jsx-scope': 'off'
+      'project-structure/independent-modules': 'error',
+      'sort-imports': 'off'
     },
     settings: {
       'project-structure/independent-modules-config-path':
@@ -52,6 +84,6 @@ const eslintConfig = [
       ...perfectionist.configs['recommended-natural'].settings
     }
   })
-]
+];
 
-export default eslintConfig
+export default eslintConfig;
